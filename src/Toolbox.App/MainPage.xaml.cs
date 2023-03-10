acd878ffbd35fa560de33df9ad498094d8fd51b9
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-03-10 19:14:10 +0800

    创建 APP 项目和 API 项目模板

---
+++ src/Toolbox.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Toolbox.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Toolbox.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Toolbox.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
