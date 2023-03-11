b76471b0ac125168f610dbc2082dcde4c292a7d5
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-03-11 20:46:34 +0800

    迁移 ChatGPT

---
+++ src/Toolbox.Pages/Models/ApiResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Toolbox.Pages.Models;
+
+public class ApiResult
+{
+    public int Code { get; set; }
+    public string Message { get; set; } = "";
+}
+
+public class ApiResult<T>
+{
+    public int Code { get; set; }
+    public string Message { get; set; } = "";
+    public T? Data { get; set; }
+}
---
+++ src/Toolbox.Pages/Models/QuestionAndAnswer.cs
@@ -0,0 +1,12 @@
+﻿namespace Toolbox.Pages.Models;
+public class QuestionAndAnswer
+{
+    public string Question { get; set; }
+    public string Answer { get; set; }
+
+    public QuestionAndAnswer(string question, string answer)
+    {
+        Question = question;
+        Answer = answer;
+    }
+}
