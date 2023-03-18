b3879381a7243b29da395ef1b11a48b3f97166b4
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-03-18 19:06:01 +0800

    完成哈希页面

---
+++ src/Toolbox.Pages/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+} 
